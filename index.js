536ea3c2ab00dc6434ee05f6f5d476f057285a51
Kevina <kevinanakayiza@gmail.com>
2025-09-12 13:43:37 +0300

    Have created a for loop that checks if variable are has know question and if yes, it will add one question to be answered

--- index.js
+++ index.js
@@ -1,80 +1,39 @@
-const question1 = {
-    question: "Which keyword is used to declare a variable in JavaScript that cannot be reassigned?",
-    optiona : "var",
-    optionb : "let",
-    optionc : "const",
-    optiond : "assign",
-        
-    }
-    
-    
 
+const questions = [
+   {
+    question : "Which keyword is used to declare a variable in JavaScript that cannot be reassigned?",
+    options :{a : "var", b : "let", c : "const", d : "assign"},
+    correct : "c",
+   },
 
-function JStest (attempts){
-    alert("Question 1");
+  {
+    question : "What does JS stand for in programming?",
+    options : {a : "JavaScript", b : "JavaStyle", c : "JargonSyntax", d : "JustScript"},
+    correct : "a"
+  },
 
-const answer = prompt(
-  "1. Which keyword is used to declare a variable in JavaScript that cannot be reassigned?\n" +
-  "a) var\n" +
-  "b) let\n" +
-  "c) const\n" +
-  "d) assign"
-);
+  {
+    question: "Which method is used to print output to the browser console in JavaScript?",
+    options:{a : "Print()", b : "echo()", c : "write()", d : "console.log()"},
+    correct : "d"
+  }
+]
 
-if (answer === "c" || answer === question1.optionc ){
-    console.log(`${question1.optionc} is correct`);
+for (K = 0; K < questions.length; K++){
+    const iterate = questions[K];
+    const answer = prompt (
+        `${K+1}.${iterate.text} \n` +
+        `a) ${iterate.options.a} \n` +
+        `b) ${iterate.options.b} \n` +
+         `c) ${iterate.options.c} \n` +
+          `d) ${iterate.options.d} \n` 
+         );
+if(answer === iterate.correct || answer === iterate.options[iterate.correct]){
+    console.log(`${iterate.options[iterate.correct]} is correct`);
 } else {
-    console.log(`Sorry the right answer is ${question1.optionc}`)
+    console.log(`The correct answer is ${iterate.options[iterate.correct]}`)
+}
+   
 }
 
-const question2 ={
-    question: "What does JS stand for in programming?",
-    optiona : "JavaScript",
-    optionb : "JavaStyle",
-    optionc : "JargonSyntax",
-    optiond : "JustScript",
 
-}
-
-const answer2 = prompt (
-    "2. What does JS stand for in programming? \n" +
-    "a) JavaScript \n" + 
-    "b) JavaStyle \n" +
-    "c) JargonSyntax \n" +
-    "d) JustScript"
-
-)
-
-if(answer2 === "a" || answer2 === question2.optiona){
-    console.log(`${question2.optiona} is correct`);
-} else {
-    console.log(`Sorry the right answer is ${question2.optiona}`)
-
-}
-
-const question3 = {
-    question : "Which method is used to print output to the browser console in JavaScript?",
-    optiona : "print",
-    optionb : "echo",
-    optionc : "write",
-    optiond : "console.log",
-
-}
-
-const answer3 = prompt (
-    "3. Which method is used to print output to the browser console in JavaScript? \n" +
-    "a) print() \n" + 
-    "b) echo \n" +
-    "c) write \n" +
-    "d) console.log()"
-
-)
-
-if(answer3 === "d" || answer3 === question3.optiond){
-    console.log(`${question3.optiond} is correct`);
-} else {
-    console.log(`Sorry the right answer is ${question3.optiond}`)
-}
-}
-
-alert (JStest)
