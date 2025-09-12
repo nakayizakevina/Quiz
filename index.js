371e8dade3a3a3855ab91cd17e8a4598aaae1886
Kevina <kevinanakayiza@gmail.com>
2025-09-12 14:12:24 +0300

    Created an if statement that checks if the answer the learner has provided is correct by entering a letter  or by enter the real word or answer

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const questions = [
 for (K = 0; K < questions.length; K++){
     const iterate = questions[K];
     const answer = prompt (
-        `${K+1}.${iterate.text} \n` +
+        `${K+1}.${iterate.question} \n` +
         `a) ${iterate.options.a} \n` +
         `b) ${iterate.options.b} \n` +
-         `c) ${iterate.options.c} \n` +
-          `d) ${iterate.options.d} \n` 
+        `c) ${iterate.options.c} \n` +
+        `d) ${iterate.options.d} \n` 
          );
-if(answer === iterate.correct || answer === iterate.options[iterate.correct]){
+ if(answer === iterate.correct || answer === iterate.options[iterate.correct]){
     console.log(`${iterate.options[iterate.correct]} is correct`);
 } else {
     console.log(`The correct answer is ${iterate.options[iterate.correct]}`)
@@ -36,4 +36,5 @@ if(answer === iterate.correct || answer === iterate.options[iterate.correct]){
    
 }
 
+        
 
